88c1f6719c999920d8f2fc71240b8c83fcafe2f8
dbchau10 <82007790+dbchau10@users.noreply.github.com>
2026-03-16 00:42:04 +0700

    set up env

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+   <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+    <h1 className="text-3xl font-bold text-blue-600">
+      Điểm danh lớp học
+    </h1>
+   </div>
+  )
+}
+
+export default App
